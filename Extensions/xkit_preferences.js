e3de16be3b7be1eb5cf1c20247d0ff36d327ee8a
April Sylph <april@sylph.tech>
2020-07-06 11:34:23 +0100

    remove Auto Load Images (#1901)

--- Extensions/xkit_preferences.js
+++ Extensions/xkit_preferences.js
@@ -1,5 +1,5 @@
 //* TITLE XKit Preferences **//
-//* VERSION 7.6.6 **//
+//* VERSION 7.6.7 **//
 //* DESCRIPTION Lets you customize XKit **//
 //* DEVELOPER new-xkit **//
 
@@ -172,7 +172,7 @@ XKit.extensions.xkit_preferences = new Object({
 
 	spring_cleaning: function() {
 
-		var clean_list = ["unreverse", "filter_by_type", "XIM", "yahoo", "reblog_as_text", "reblog_yourself", "alternative_timestamps"];
+		var clean_list = ["unreverse", "filter_by_type", "XIM", "yahoo", "reblog_as_text", "reblog_yourself", "alternative_timestamps", "autoloadimages"];
 
 		var removed_list = [];
 
